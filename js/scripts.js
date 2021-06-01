5cf47a4337dc4f9f148a9b0c77def7bda9e2cdf0
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-01 15:50:56 -0700

    Flashcard site practice: More jQuery Basics lesson 37 of 61

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  
+  $("#clickable1").click(function(){
+    $(".card-title#num1").toggle();
+    $(".hidden#num1").toggle();
+  });
+
+  $("#clickable2").click(function(){
+    $(".card-title#num2").toggle();
+    $(".hidden#num2").toggle();
+  });
+
+  $("#clickable3").click(function(){
+    $(".card-title#num3").toggle();
+    $(".hidden#num3").toggle();
+  });
+
+  $("#clickable4").click(function(){
+    $(".card-title#num4").toggle();
+    $(".hidden#num4").toggle();
+  });
+
+  $("#clickable5").click(function(){
+    $(".card-title#num5").toggle();
+    $(".hidden#num5").toggle();
+  });
+
+  $("#clickable6").click(function(){
+    $(".card-title#num6").toggle();
+    $(".hidden#num6").toggle();
+  });
+
+  $("#clickable7").click(function(){
+    $(".card-title#num7").toggle();
+    $(".hidden#num7").toggle();
+  });
+
+  $("#clickable8").click(function(){
+    $(".card-title#num8").toggle();
+    $(".hidden#num8").toggle();
+  });
+
+  $("#clickable9").click(function(){
+    $(".card-title#num9").toggle();
+    $(".hidden#num9").toggle();
+  });
+
+  $("#clickable10").click(function(){
+    $(".card-title#num10").toggle();
+    $(".hidden#num10").toggle();
+  });
+
+  $("#clickable11").click(function(){
+    $(".card-title#num11").toggle();
+    $(".hidden#num11").toggle();
+  });
+
+  $("#clickable12").click(function(){
+    $(".card-title#num12").toggle();
+    $(".hidden#num12").toggle();
+  });
+
+});
